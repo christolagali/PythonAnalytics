04ff71855609cd8bd6686e0b04d59644a0bad3ff
Christo <christo.lagali@gmail.com>
2017-02-24 22:55:23 -0800

    Created connectToSql.py file: 1. To establish connection with SQL Server 2. Create con (Connection object) and cur(Cursor Obejct) 3. This file will be imported to access Sql Connection ------------------------------------------------------------------------ Created moveJsonToSql.py file: 1. To read JSON file into an object 2. Establish connection with SQL Server for Insert query

---
+++ moveJsonToSql.py
@@ -0,0 +1,24 @@
+import json
+from pprint import pprint
+import connectToSql
+with open('lease.json') as data_file:
+    data = json.load(data_file)
+
+pprint(data[0])
+
+cur = connectToSql.con.cursor()
+
+
+cur.execute("insert into dbo.mydummytable(myid,myname,myage,mycity) values (?,?,?,?)",'8','Jayant','32','Pune')
+connectToSql.con.commit()
+
+cur.execute("select myid,myname,myage,mycity from dbo.mydummytable")
+for row in cur:
+    print(row.myid , "," , row.myname)
+    #print row[0] + "," + row[1]
+
+cur.close()
+connectToSql.con.close()
+
+# Maybe we could check the file size
+# if filesize is >2048 then we do the buffersize else line by line
