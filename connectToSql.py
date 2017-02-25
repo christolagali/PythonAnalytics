2d6a48e7e2a1aa2b054da8fb0aeda1c96d6b1775
Christo <christo.lagali@gmail.com>
2017-02-24 22:48:52 -0800

    Created connectToSql.py file: 1. To establish connection with SQL Server 2. Create con (Connection object) and cur(Cursor Obejct) 3. This file will be imported to access Sql Connection

---
+++ connectToSql.py
@@ -0,0 +1,12 @@
+import pyodbc
+
+print("I reached here")
+#con = pyodbc.connect("DRIVER={SQL Server};server=//CHRISTO-ADMIN\SQLEXPRESS;port=1433;database=sftodbexample;uid=chris;pwd=PraiseLord77")
+
+theserver = "//CHRISTO-ADMIN\SQLEXPRESS:1433"
+thedatabase = "sftodbexample"
+theuser = "chris"
+thepassword="PraiseLord77"
+
+#con = pyodbc.connect(driver='{SQL Server Native Client 11.0}',host=theserver,database=thedatabase,trusted_connection='yes',user=theuser,password=thepassword)
+con=pyodbc.connect('DRIVER={SQL Server};SERVER=CHRISTO-ADMIN\SQLEXPRESS;PORT=1433;DATABASE=sftodbexample;UID=chris;PWD=PraiseLord77')
