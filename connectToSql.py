f55adfe16b7b64d36105fac1f49a668ad41735c0
Christo <christo.lagali@gmail.com>
2017-02-25 19:56:10 -0800

    Created connectToSql.py file: 1. To establish connection with SQL Server 2. Create con (Connection object) and cur(Cursor Obejct) 3. This file will be imported to access Sql Connection 4. Implemented Class structure and created ConnectToSql class 5. Class houses functions connect() for SQL connection and insertValues() for inserting JSON data ------------------------------------------------------------------------ Created moveJsonToSql.py file: 1. To read JSON file into an object 2. Establish connection with SQL Server for Insert query 3. Access to class variables were established 4. Call to functions connect() and insertValues()

--- connectToSql.py
+++ connectToSql.py
@@ -1,12 +1,24 @@
 import pyodbc
 
-#print("I reached here")
-#con = pyodbc.connect("DRIVER={SQL Server};server=//CHRISTO-ADMIN\SQLEXPRESS;port=1433;database=sftodbexample;uid=chris;pwd=PraiseLord77")
+class ConnectToSql():
+    #print("I reached here")
+    #con = pyodbc.connect("DRIVER={SQL Server};server=//CHRISTO-ADMIN\SQLEXPRESS;port=1433;database=sftodbexample;uid=chris;pwd=PraiseLord77")
 
-theserver = "//CHRISTO-ADMIN\SQLEXPRESS:1433"
-thedatabase = "sftodbexample"
-theuser = "chris"
-thepassword="PraiseLord77"
+    theserver = "//CHRISTO-ADMIN\SQLEXPRESS:1433"
+    thedatabase = "sftodbexample"
+    theuser = "chris"
+    thepassword="PraiseLord77"
 
-#con = pyodbc.connect(driver='{SQL Server Native Client 11.0}',host=theserver,database=thedatabase,trusted_connection='yes',user=theuser,password=thepassword)
-con=pyodbc.connect('DRIVER={SQL Server};SERVER=CHRISTO-ADMIN\SQLEXPRESS;PORT=1433;DATABASE=sftodbexample;UID=chris;PWD=PraiseLord77')
+    def connect(self):
+        self.con=pyodbc.connect('DRIVER={SQL Server};SERVER=CHRISTO-ADMIN\SQLEXPRESS;PORT=1433;DATABASE=sftodbexample;UID=chris;PWD=PraiseLord77')
+
+
+    def insertValues(self,data):
+        cur = self.con.cursor()
+        for item in data:
+            if item['soldVehicleVin'] == 'none':
+                item['soldVehicleVin'] = 'null'
+            cur.execute("insert into dbo.Vehicle_Sales_Table(Manufacturer,[Current Vehicle Vin],[Customer Number],[Dealer Id],[Has Sale],[Month]) values (?,?,?,?,?,?)",item['Manufacturer'],item['currentVehicleVin'],item['customerNumber'],item['dealerId'],item['hasSale'],item['month'])
+            self.con.commit()
+        print('Value inserted!')
+        cur.close()
--- moveJsonToSql.py
+++ moveJsonToSql.py
@@ -1,21 +1,40 @@
 import json
 from pprint import pprint
 import connectToSql
+
+
+#load and read from JSON file
 with open('lease.json') as data_file:
     data = json.load(data_file)
 
+
+    #event = max(data,key=lambda ev:ev['customerNumber'])
+    #print('Max Value',event)
+    #event = min(data, key=lambda ev: ev['customerNumber'])
+    #print('Min Value', event)
+
+#looping through json obejct data
+
+#for item in data:
+#    value = item['currentVehicleVin']
+    #print(value)
+#    i = i + 1
+#    if value == 3396933746077368191:
+#        print(item['currentVehicleVin'], item['customerNumber'])
+
 #pprint(data[0])
 
-cur = connectToSql.con.cursor()
-cur.execute("insert into dbo.mydummytable(myid,myname,myage,mycity) values (?,?,?,?)",'8','Jayant','32','Pune')
-connectToSql.con.commit()
+Connectvar = connectToSql.ConnectToSql
 
-cur.execute("select myid,myname,myage,mycity from dbo.mydummytable")
-for row in cur:
-    print(row.myid , "," , row.myname)
+Connectvar.connect(Connectvar)
+Connectvar.insertValues(Connectvar,data)
 
-cur.close()
-connectToSql.con.close()
+#cur.execute("select myid,myname,myage,mycity from dbo.mydummytable")
+#for row in cur:
+#    print(row.myid , "," , row.myname)
+
+#cur.close()
+Connectvar.con.close()
 
 # Maybe we could check the file size
 # if filesize is >2048 then we do the buffersize else line by line
